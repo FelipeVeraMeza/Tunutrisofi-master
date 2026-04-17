2d9475bde4a7314d2142a820b7a0997fe99f7287
Felipe <felipe.veram2001@gmail.com>
2026-04-17 16:05:45 -0400

    prueba 1

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const IP_PUBLICA = 'tunutrisofi-master-production.up.railway.app';
+const IP_PUBLICA = 'https://tunutrisofi-master.onrender.com';
 
 //export const API_BASE_URL = 'http://localhost:4000/api';
 export const API_BASE_URL = `https://${IP_PUBLICA}/api`;
