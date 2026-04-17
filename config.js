24e1116000f7518f08fbc2ff4eedb28908611cb9
Felipe <felipe.veram2001@gmail.com>
2026-04-17 15:53:54 -0400

    funciona

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const IP_PUBLICA = 'tunutrisofi-master-production.up.railway.app';
 
 //export const API_BASE_URL = 'http://localhost:4000/api';
 export const API_BASE_URL = `https://${IP_PUBLICA}/api`;
-export const FRONTEND_URL = `https://vsv-contadores.vercel.app`;
+export const FRONTEND_URL = `https://tunutrisofi-master.vercel.app`;
