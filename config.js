bd5f0bc05408fc44eddd35bef7358e9a55044577
Felipe <felipe.veram2001@gmail.com>
2026-05-02 01:44:07 -0400

    02-05

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const IP_PUBLICA = 'https://nutrisofi.onrender.com';
 
-export const API_BASE_URL = 'http://localhost:4000/api';
-//export const API_BASE_URL = `https://${IP_PUBLICA}/api`;
+//export const API_BASE_URL = 'http://localhost:4000/api';
+export const API_BASE_URL = `https://${IP_PUBLICA}/api`;
 export const FRONTEND_URL = `https://tunutrisofi.vercel.app`;
